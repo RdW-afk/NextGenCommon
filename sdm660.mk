3dd5160d46f7ca9829272a09b0cd2e900ad9e390
xiaoleGun <1592501605@qq.com>
2022-01-08 12:42:51 +0800

    sdm660-common: Add prebuilt Via Browser
    * From https://github.com/tuyafeng/Via
    * From https://res.viayoo.com/v1/via-release-cn.apk
    MD5: 69267937ad1546091273d7e3b128258e

--- sdm660.mk
+++ sdm660.mk
@@ -80,6 +80,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Browser
+PRODUCT_PACKAGES += \
+     Via
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
