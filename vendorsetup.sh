c33d2187a23fdac24176e714e468cf8d50d92411
Romeo <rbesar210311@gmail.com>
2023-02-19 21:59:01 +0800

    sdm660-common: Regenerate vendorsetup
    Signed-off-by: Romeo <rbesar210311@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,31 @@
-# Audio
-rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/wizardprjkt/hardware_qcom-caf_msm8998_audio hardware/qcom-caf/msm8998/audio
+echo '----------------------------------'
+echo '| Starting to clone stuffs needed |'
+echo '----------------------------------'
 
-# ih8sn
+echo '--------------------'
+echo '| Cloning Audio HALs |'
+echo '--------------------'
+rm -rf hardware/qcom-caf/msm8998/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-20.0-caf-msm8998 hardware/qcom-caf/msm8998/audio
+
+echo '--------------------'
+echo '| Cloning Display HALs |'
+echo '--------------------'
+rm -rf hardware/qcom-caf/msm8998/display
+git clone https://github.com/LineageOS/android_hardware_qcom_display -b lineage-20.0-caf-msm8998 hardware/qcom-caf/msm8998/display
+
+echo '--------------------'
+echo '| Cloning Media HALs |'
+echo '--------------------'
+rm -rf hardware/qcom-caf/msm8998/media
+git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-20.0-caf-msm8998 hardware/qcom-caf/msm8998/media
+
+echo '--------------------'
+echo '| Cloning ih8sn HALs |'
+echo '--------------------'
 rm -rf vendor/ih8sn
 git clone https://github.com/Terminator-J/vendor_ih8sn -b 12.1-test vendor/ih8sn
 
+echo '-------------------------'
+echo '| Cloning all stuffs success |'
+echo '-------------------------'
