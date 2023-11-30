e716dcfa2ad83dde02a2e8b0347b7d468a31e2c7
Mr_ZerO <rbesar210311@gmail.com>
2023-11-30 03:14:06 +0000

    X00TD: Build GalleryPhotoManager
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -165,6 +165,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+# Gallery
+PRODUCT_PACKAGES += \
+    GalleryPhotoManager
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
